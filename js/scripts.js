d2bcfde8e509540346f12079c516fa404a92c2c3
Anurag Shamsher Rana <anurag@sherpa-tech.com>
2024-01-19 12:36:26 +0545

    initial

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,45 @@ $(document).ready(function () {
     $(this).addClass("active").siblings().removeClass("active");
   });
 });
+
+// dynamic color
+function hexToRgb(hex) {
+  hex = hex.replace(/^#/, "");
+  const bigint = parseInt(hex, 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+
+  return `${r}, ${g}, ${b}`;
+}
+
+const priColor = "#55a960";
+const secColor = "#022c46";
+const priRGB = hexToRgb(priColor);
+const secRGB = hexToRgb(secColor);
+
+document.documentElement.style.setProperty("--pri", priColor);
+document.documentElement.style.setProperty("--sec", secColor);
+document.documentElement.style.setProperty("--prirgb", priRGB);
+document.documentElement.style.setProperty("--secrgb", secRGB);
+
+// video Popup
+const videoPopup = document.querySelector(".video-popup");
+const videoPopupOpen = document.querySelector(".video-play-button");
+const videoPopupBg = document.querySelector(".popup-bg");
+const videoPopupClose = document.querySelector(".popup-close");
+
+videoPopupOpen.addEventListener("click", () => {
+  videoPopup.classList.add("active");
+  videoPopup.querySelector("iframe").style.display = "block";
+});
+
+videoPopupBg.addEventListener("click", () => {
+  videoPopup.classList.remove("active");
+  videoPopup.querySelector("iframe").style.display = "none";
+});
+
+videoPopupClose.addEventListener("click", () => {
+  videoPopup.classList.remove("active");
+  videoPopup.querySelector("iframe").style.display = "none";
+});
